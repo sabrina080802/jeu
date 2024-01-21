594d59b28d333830a97c08b1f2052b6a3a5ab0f1
Max <m.antonio53100@gmail.com>
2024-01-21 12:48:28 +0100

    Passage du framework en full objet (à tester)

--- src/js/magy.js
+++ src/js/magy.js
@@ -1,110 +1,105 @@
 const app = new Magy();
 
-function Magy() {
-  const self = this;
-  let isReady = false;
-  let waitingCalls = [];
-  new EventHandler(self);
+class Magy {
+    #waitingCalls = [];
+    #isReady = false;
 
-  document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
-
-  this.run = (callback) => {
-    if (isReady) callback();
-    else {
-      waitingCalls.push(callback);
+    constructor() {
+        document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+        this.http = new MagyHttpBuilder();
     }
-  };
-  this.wait = (time) => {
-    return new Promise((resolve) => {
-      setTimeout(resolve, time);
-    });
-  };
-  this.request = async (url, data, method = "POST") => {
-    try {
-      const formData = new FormData();
-      for (const key in data) {
-        formData.append(key, data[key]);
-      }
-
-      // Options de la requête
-      const requestOptions = {
-        method: method,
-        body: formData,
-      };
-
-      // Utilisation de fetch pour envoyer la requête POST
-      const response = await fetch(url, requestOptions);
-      if (!response.ok) {
-        throw new Error("La requête a échoué.");
-      }
-
-      return await response.json();
-    } catch (error) {
-      return null;
+    onDOMContentLoaded() {
+        this.#isReady = true;
+        this.#waitingCalls.forEach(callback => callback());
     }
-  };
-
-  this.loadEntity = (name) => {};
-
-  function onDOMContentLoaded() {
-    isReady = true;
-    waitingCalls.forEach((x) => {
-      x();
-    });
-    waitingCalls = null;
-  }
+    run(callback) {
+        if (this.#isReady) {
+            callback();
+        }
+        else {
+            this.#waitingCalls.push(callback);
+        }
+    }
 }
 
-function EventHandler(target) {
-  const self = this;
-  const storedEvents = [];
+class MagyHttpBuilder {
+    async request(url, data, method = "POST") {
+        try {
+            const formData = new FormData();
+            for (const key in data) {
+                formData.append(key, data[key]);
+            }
 
-  this.addListener = (eventName, callback, options) => {
-    if (!storedEvents[eventName]) {
-      storedEvents[eventName] = [];
+            // Options de la requête
+            const requestOptions = {
+                method: method,
+                body: formData,
+            };
+
+            // Utilisation de fetch pour envoyer la requête POST
+            const response = await fetch(url, requestOptions);
+            if (!response.ok) {
+                throw new Error("La requête a échoué.");
+            }
+
+            return await response.json();
+        } catch (error) {
+            return null;
+        }
     }
-    storedEvents[eventName].push({ callback, options });
-  };
-
-  this.removeListener = (eventName, callback) => {
-    if (!storedEvents[eventName]) return;
-
-    const eList = storedEvents[eventName];
-    for (let i = 0; i < eList.length; i++) {
-      if (eList[i].callback === callback) {
-        eList.splice(i, 1);
-        return;
-      }
-    }
-  };
-
-  this.dispatchEvent = (eventName, ...data) => {
-    if (!storedEvents[eventName]) return;
-
-    const awaiters = [];
-    const eList = storedEvents[eventName];
-    for (let i = 0; i < eList.length; i++) {
-      if (!eList[i].callback) continue;
-
-      const value = eList[i].callback(...data);
-      if (value instanceof Promise) {
-        awaiters.push(value);
-      }
-    }
-
-    return awaiters;
-  };
 }
 
-function Point(x = 0, y = 0) {
-  const self = this;
-  this.x = x;
-  this.y = y;
+class EventHandler {
+    #storedEvents = [];
 
-  this.distanceTo = function (point) {
-    return Math.sqrt(
-      (point.x - self.x) * (point.x - self.x) +
-        (point.y - self.y) * (point.y - self.y)
-    );
-  };
+    addListener(eventName, callback, options) {
+        if (!this.#storedEvents[eventName]) {
+            this.#storedEvents[eventName] = [];
+        }
+        this.#storedEvents[eventName].push({ callback, options });
+    }
+    removeListener(eventName, callback) {
+        if (!this.#storedEvents[eventName]) return;
+        const eList = this.#storedEvents[eventName];
+        for (let i = 0; i < eList.length; i++) {
+            if (eList[i].callback == callback) {
+                eList.splice(i, 1);
+                return;
+            }
+        }
+    }
+    dispatchEvent(eventName, ...args) {
+        if (!this.#storedEvents[eventName]) return;
+
+        const awaiters = [];
+        const eList = this.#storedEvents[eventName];
+        for (let i = 0; i < eList.length; i++) {
+            if (!eList[i].callback) continue;
+            const value = eList[i].callback(...args);
+            if (value instanceof Promise) {
+                awaiters.push(value);
+            }
+            if (eList[i].options && eList[i].options.once === true) {
+                eList.splice(i--, 1);
+            }
+        }
+
+        return awaiters;
+    }
+}
+
+class Point {
+    x = 0;
+    y = 0;
+
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+    distanceTo(point) {
+        return Math.sqrt(
+            (point.x - this.x) * (point.x - this.x) +
+            (point.y - this.y) * (point.y - this.y)
+        );
+    }
 }
